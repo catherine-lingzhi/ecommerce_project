109aebf1c5ea10c514a9862f571483e5a4cb8115
Lingzhi Luo <lingzhiluo8@outlook.com>
2023-11-13 00:12:09 -0600

    Add ProductStatus Model and add Reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_13_055742) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_13_061016) do
   create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_statuses", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -25,8 +31,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_13_055742) do
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "product_status_id", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["product_status_id"], name: "index_products_on_product_status_id"
   end
 
   add_foreign_key "products", "categories"
+  add_foreign_key "products", "product_statuses"
 end
