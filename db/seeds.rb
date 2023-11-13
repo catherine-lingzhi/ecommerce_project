a3413b30cf22676237ce6326986014c6dd7652df
Lingzhi Luo <lingzhiluo8@outlook.com>
2023-11-13 00:54:01 -0600

    Add 200 products and 10 cateogories and 2 prodcut statsu

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
 require "net/http"
 require "json"
 require "uri"
-
 Product.delete_all
 Category.delete_all
+ProductStatus.delete_all
+
+# Create ProductStatus Table
+statuses = ["New", "On Sale", "Recently Updated"]
+
+statuses.each do |status_name|
+  ProductStatus.find_or_create_by(name: status_name)
+end
 
 # Helper function to validate image link
 def valid_image_link?(image_link)
@@ -20,27 +27,43 @@ def description_has_html_tag?(description)
   !!(description =~ %r{</?[a-z][\s\S]*>})
 end
 
-# Fetch API to populate data to database
+# Fetch API to populate data to the database
 url = "http://makeup-api.herokuapp.com/api/v1/products.json"
 uri = URI(url)
 response = Net::HTTP.get(uri)
 data = JSON.parse(response)
 
+# Fetch the default status "New"
+new_status = ProductStatus.find_by(name: "New")
+
+unless new_status
+  puts "Creating 'New' status..."
+  new_status = ProductStatus.create(name: "New")
+end
+
+# Counter to limit the number of products
+products_created = 0
+desired_product_limit = 200
+
 data.each do |product_data|
+  break if products_created >= desired_product_limit
+
   category = Category.find_or_create_by(name: product_data["product_type"])
 
   if category && category.valid?
     image_link = product_data["image_link"]
 
     if valid_image_link?(image_link) && !description_has_html_tag?(product_data["description"])
-
+      # db/seeds.rb
       product = category.products.create(
-        name:        product_data["name"],
-        price:       Faker::Commerce.price,
+        name:           product_data["name"],
+        price:          product_data["price"],
         image_link:,
-        description: product_data["description"]
+        description:    product_data["description"],
+        product_status: ProductStatus.find_by(name: statuses.sample) # Randomly assign a product status
       )
 
+      products_created += 1
     else
       puts "Invalid image link: #{image_link}"
     end
@@ -50,7 +73,5 @@ data.each do |product_data|
 end
 
 puts "Created #{Category.count} categories"
-puts "Created #{Brand.count} brands"
 puts "Created #{Product.count} products"
-puts "Created #{Tag.count} tags"
-puts "Created #{ProductTag.count} product tags"
+puts "Created #{ProductStatus.count} product Status"
